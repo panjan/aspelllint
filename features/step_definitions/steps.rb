4ade186b4e4371677cff327b6bf362c95146726f
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-14 10:57:53 -0500

    line number typo

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,6 +7,6 @@ Then(/^the output is correct for each test$/) do
 
   lines.length.should == 2
 
-  lines[0].should =~ %r(^examples/nested/memo\.md\:18\:20\sFribsday\:\s(.+,\s)+.+$)
+  lines[0].should =~ %r(^examples/nested/memo\.md\:17\:20\sFribsday\:\s(.+,\s)+.+$)
   lines[1].should =~ %r(^examples/toy-boats\.txt\:46\:11\sbaots\:\s(.+,\s)+.+$)
 end
